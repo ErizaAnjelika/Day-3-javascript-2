ffe1f89d97acef585ff05171190ac16a91d3c952
ErizAnjelika <erizaangel5@gmail.com>
2023-08-31 19:36:29 +0700

    first commit

---
+++ tipe-data.js
@@ -0,0 +1,134 @@
+//Memanggil Variabel
+/*let variabel1 = "Eriza",
+variabel2 = 12345,
+variabel3 = "Saya Belajar Javascript";
+
+console.log(`${variabel1} ${variabel2} ${variabel3}`);
+*/
+
+//Menentukan tipe data
+/*var nama1 = "Eriza"
+let nama2 = "Eriza Anjelika"
+const nama3 = "Eriza ANjelika";
+nama1 = 123456
+console.log(typeof(nama1));*/
+
+
+//contoh undifined
+/*let nama;
+// console.log(nama);*/
+
+
+//Kode dengan suatu kondisi
+// var username1 = "eriza";
+// var usia = 20;
+// var batas = 15;
+// if (usia > batas) {
+//     var username1="nafa";
+//     console.losg (`usia ${username1} Lebih dari ${batas} `)
+// }else{
+//     console.log(`usia ${username1} kurang dari ${batas}`)
+// }
+
+
+// let username1 = "eriza";
+// let usia = 20;
+// let batas = 15;
+// if (usia > batas) {
+//     let username1="nafa";
+//     let statusPelajar = true;
+//     console.log (`usia ${username1} Lebih dari ${batas} ${statusPelajar} `)
+// }else{
+//     console.log(`usia ${username1} kurang dari ${batas} ${statusPelajar}`)
+// }
+
+// let warna = ["merah", "kuning", "biru"]; //array
+
+// let siswa ={
+//     nama : "eriza",
+//     usia : "20",
+//     statusPelajar : true,
+// }
+
+// const array =[
+//     "string",
+//     false,
+//     12345,
+//     {key1 :"value1", key2: "value2"},
+//     function(){
+//         console.log("ini function");
+//     },
+// ];
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[3]);
+
+// const dataSiswa =[
+//     {
+//         nama : "eriza",
+//         kelas : 12,
+//         usia : 2022,
+//     },
+//     {
+//         nama : "nafa'",
+//         kelas : 12,
+//         usia : 2022,
+//     }
+// ]
+// for (let i = 0; i < dataSiswa.length; i++) {
+//     for (let j = 0; j < dataSiswa.length; j++) {
+//         const element = array[j];
+//     }
+//     console.log(dataSiswa[i]);
+// }
+
+
+//Contoh array
+// const dataSiswa = [
+//     {
+//         name: "lala",
+//         kelas: "1A",
+//         tahunMasuk: 2023,
+//     },
+//     {
+//         name: "lili",
+//         kelas: "1A",
+//         tahunMasuk: 2023,
+//     },
+// ]
+
+//Kode ini masih error jangan dicoba!!
+// for (let i = 0; i < dataSiswa.length; i++) {
+//     for (let j = 0; j < dataSiswa[0].length; j++) {
+//         console.log(i, j);
+//     }
+// }
+
+
+//Operator perbandingan
+// let num1 = 10;
+// let num2 = '5';
+// console.log(num1 == num2);
+// console.log(num1 != num2);
+
+
+//operator logika &&, ||, !
+//username condition
+// -min 3 karakter
+
+//password 
+//password min 5 karakter
+//12345
+
+// let username = true;
+// let password = true;
+// if (username != password) {
+//     console.log("Anda berhasil login");
+// }else{
+//     console.log("Anda Gagal Login");
+// }
+
+let num1 = 1;
+a = num1++;
+console.log(a); 
